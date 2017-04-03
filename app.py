09dd86ee4dfd804616099caa9bb0c99cc32d0f66
Darran <dkopm95.12@gmail.com>
2017-04-03 12:38:40 +0100

    Got retrieve comment working

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 import httplib2
 import os
+import pprint
 
 from apiclient import discovery
 from googleapiclient import errors
@@ -8,22 +9,19 @@ from oauth2client import client
 from oauth2client import tools
 from oauth2client.file import Storage
 from flask import Flask, render_template
+
 try:
     import argparse
+
     flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
 except ImportError:
     flags = None
 
-# If modifying these scopes, delete your previously saved credentials
-# at ~/.credentials/drive-python-quickstart.json
-SCOPES = 'https://www.googleapis.com/auth/drive.metadata.readonly'
+SCOPES = 'https://www.googleapis.com/auth/drive, https://www.googleapis.com/auth/drive.file'
 CLIENT_SECRET_FILE = 'client_secret.json'
 APPLICATION_NAME = 'Drive API Python Quickstart'
 app = Flask(__name__)
 
-@app.route('/')
-def welcome():
-    return render_template('index.html')
 
 def get_credentials():
     home_dir = os.path.expanduser('~')
@@ -31,7 +29,7 @@ def get_credentials():
     if not os.path.exists(credential_dir):
         os.makedirs(credential_dir)
     credential_path = os.path.join(credential_dir,
-                                   'drive-python-quickstart.json')
+                                   'drive-python-commentCollector.json')
 
     store = Storage(credential_path)
     credentials = store.get()
@@ -42,21 +40,36 @@ def get_credentials():
             credentials = tools.run_flow(flow, store, flags)
     return credentials
 
-@app.route('/authentication' ,methods=['POST'])
-def main():
-    credentials = get_credentials()
-    http = credentials.authorize(httplib2.Http())
-    service = discovery.build('drive', 'v3', http=http)
-    file_id = '1O79FEH8hY6I2FjwXUivuzmSFJ9D0VM5l7BJ5WDGk_MQ'
-    results = service.comments().list(fileId=file_id).execute()
-    items = results.get('files', [])
-    if not items:
-        print('No files found.')
-    else:
-        print('Files:')
-        for item in items:
-            print('{0} ({1})'.format(item['name'], item['id']))
-    return render_template('index.html'), results
+
+credentials = get_credentials()
+http = credentials.authorize(httplib2.Http())
+service = discovery.build('drive', 'v3', http=http)
+
+
+@app.route('/')
+def welcome():
+    return render_template('index.html')
+
+
+@app.route('/authentication', methods=['POST'])
+def retrieve_comments():
+    id =get_fileid()
+    try:
+        comments = service.comments().list(fileId='1O79FEH8hY6I2FjwXUivuzmSFJ9D0VM5l7BJ5WDGk_MQ',
+                                           fields="comments").execute()
+        c=comments.get('comments', [])
+        print(c)
+    except errors.HttpError as error:
+        print('An error occurred: %s' % error)
+    return render_template('index.html')
+
+
+def get_fileid():
+    results = service.files().list(
+        pageSize=1, fields="nextPageToken, files(id, name)").execute()
+    items = results.get('files')
+    print(items)
+    return items
 
 if __name__ == '__main__':
     app.run(debug=True)
